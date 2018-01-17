b1b774c4e85abc315713c95cab6671010719f04d
Northerntwig <oscar.cv.nordquist@gmail.com>
2018-01-17 17:43:35 +0100

    Rebass provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './components/App'
+import { Provider } from 'rebass'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'))
 registerServiceWorker()
