8c414e70d0bff6f85f6f45c8f7f3c800a7334995
Northerntwig <oscar.cv.nordquist@gmail.com>
2018-01-17 17:14:00 +0100

    Fetches all user necessary data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,14 +4,35 @@ import { compose, withState, lifecycle} from 'recompose'
 const enhance = compose(
   withState('users', 'setUsers', []),
   lifecycle({
-    async componentWillMount() {
+    async componentDidMount() {
       const usersData = await fetch('http://localhost:8080/users')
       const users = await usersData.json()
-      this.props.setUsers(users)
+
+
+      const allNamesPromise = Array(2).fill(0).map(async () => {
+        const namesData = await fetch('https://uinames.com/api/?amount=400')
+        const names = await namesData.json()
+        return names
+      })
+      // const allNamesPromise = [Array(2).fill(0).reduce(async (prev, b) => {
+      // }, [])]
+
+
+      const allNames = await Promise.all(allNamesPromise)
+      const combined = allNames.reduce((prev, curr) => [...prev, ...curr], [])
+
+      const completeUsers = users.map((id, index) => ({
+        id,
+        name: combined[index].name,
+        surname: combined[index].surname,
+        gender: combined[index].gender,
+      }))
+
+      this.props.setUsers(completeUsers)
     }
   })
 )
 
 export const App = enhance(({ users }) => {
-  return users.map((user) => <h1>{  user }</h1>)
+  return users.map((user) => <h1>{  user.name }</h1>)
 }) 
