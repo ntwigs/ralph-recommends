535edae53ae9af4e0c4acba241c666009cd0f700
Northerntwig <oscar.cv.nordquist@gmail.com>
2018-01-18 10:20:28 +0100

    Fancy user reveal

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,17 +1,34 @@
 import * as React from 'react'
 import { Card, Subhead, Text, Box, Button, Row, Column } from 'rebass'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import { fadeInLeft } from 'react-animations'
 
 const CustomButton = styled(Button)`
   margin-top: 10px;
 `
 
+const fadeInAnimation = keyframes`${fadeInLeft}`
+
+const CustomSubText = styled(Subhead)`
+  animation: ${fadeInAnimation} 1s ease;
+  animation-delay: 1s;
+`
+
+const CustomText = styled(Text)`
+  animation: ${fadeInAnimation} 1s ease;
+  animation-delay: 1s;
+`
+
+const CustomCard = styled(Card)`
+  animation: ${fadeInAnimation} 1s ease;
+`
+
 export const User = ({ id, name, surname, gender }) => (
-  <Card m={20}>
+  <CustomCard m={20}>
     <Box p={10}>
-      <Subhead>{name}</Subhead>
-      <Text children={surname} />
+      <CustomSubText>{name}</CustomSubText>
+      <CustomText children={surname} />
       <CustomButton children="select" />
     </Box>
-  </Card>
+  </CustomCard>
 )
