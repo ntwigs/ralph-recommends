3e7ef85c9d013994bd70d04ec431e81bfda7609f
Northerntwig <oscar.cv.nordquist@gmail.com>
2018-01-18 10:09:19 +0100

    Displays loading

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,22 @@
 import * as React from 'react'
-import { compose, withState, lifecycle} from 'recompose'
+import { compose, withState, lifecycle, branch, renderComponent } from 'recompose'
 import { User } from './User'
+import { Flex, Box, Text, Fixed} from 'rebass'
 
 const enhance = compose(
   withState('users', 'setUsers', []),
   lifecycle({
-    async componentDidMount() {
+    async componentWillMount() {
       const usersData = await fetch('http://localhost:8080/users')
       const users = await usersData.json()
 
-
-      const allNamesPromise = Array(2).fill(0).map(async () => {
-        const namesData = await fetch('https://uinames.com/api/?amount=400')
-        const names = await namesData.json()
-        return names
-      })
+      const allNamesPromise = Array(2)
+        .fill(0)
+        .map(async () => {
+          const namesData = await fetch('https://uinames.com/api/?amount=400')
+          const names = await namesData.json()
+          return names
+        })
 
       const allNames = await Promise.all(allNamesPromise)
       const combined = allNames.reduce((prev, curr) => [...prev, ...curr], [])
@@ -27,10 +29,22 @@ const enhance = compose(
       }))
 
       this.props.setUsers(completeUsers)
-    }
-  })
+    },
+  }),
+  branch(({ users }) => !(users.length), renderComponent(() => <h1>Loading</h1>)),
 )
 
-export const App = enhance(({ users }) => {
-  return users.map((user) => <User key={ user.id } { ...user }/>)
-}) 
+export const App = enhance(({ users }) => (
+  <Flex height="100vh">
+    <Box w={1 / 5} px={2}>
+      {users.map(user => <User key={user.id} {...user} />)}
+    </Box>
+    <Box w={3 / 4} px={2}>
+      <Fixed right top w={3/4} m={28}>
+        <Text p={1} color="white" bg="blue">
+          Half
+        </Text>
+      </Fixed>
+    </Box>
+  </Flex>
+))
